78c804d1bb7892b47d4d9ee29ae708d7b165c067
jeejinf <dev.jeejor@gmail.com>
2022-11-22 08:29:33 -0300

    Função POST no /contatos funcionando corretamente

---
+++ src/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, url_for
+from flask_mysqldb import MySQL
+
+def create_app():
+    from app import routes
+    routes.init_app(app)
+
+    return app
+
+app = Flask(__name__)
+
+# conexão com o banco de dados
+app.config['MYSQL_Host'] = 'localhost' # 127.0.0.1
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fatec'
+app.config['MYSQL_DB'] = 'contatos'
+
+mysql = MySQL(app)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('public/index.html')
+
+@app.route("/quemsomos")
+def quemsomos():
+    return render_template('public/quemsomos.html')
+
+@app.route("/contatos")
+def contato():
+    return render_template('public/contatos.html')
+
+@app.route('/contatos', methods=['GET', 'POST'])
+def contatos():
+    if request.method == "POST":
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['desc']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contatos(email, assunto, `desc`) VALUES (%s, %s, %s)", (email, assunto, descricao))
+       
+        mysql.connection.commit()
+        cur.close()
+    return render_template('public/contatos.html')
